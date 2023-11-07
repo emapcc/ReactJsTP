d9c2cf013b2da6c4733e94f8fc11cb17bcb49d8d
emapcc <emapuccio72@gmail.com>
2023-11-06 23:59:56 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ecommerce React
+Explicar qué hicimos, cómo lo hicimos
+Presentar el proyecto
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <h1>Holaa mi página</h1>
+    </>
+  )
+}
+
+export default App
